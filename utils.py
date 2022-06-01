d299ef571a74b92a69c8b046b26d40b5b9c92be9
Maximilinano Osorio <mosorio@isi.edu>
2022-06-01 12:24:12 -0400

    fix: unkwown is not required

--- utils.py
+++ utils.py
@@ -12,11 +12,7 @@ def create_directory(dag_id: str, _id: str) -> str:
     airflow_directory = os.path.join("/data", "airflow")
     files_directory = os.path.join(airflow_directory, "files")
     dags_directory = os.path.join(files_directory, "dags")
-    if dag_id is not None:
-        dag_directory = os.path.join(dags_directory, dag_id)
-    else:
-        dag_directory = os.path.join(dags_directory, "unknown")
-
+    dag_directory = os.path.join(dags_directory, dag_id)
     execution_directory = os.path.join(dag_directory, _id)
     if not os.path.exists(execution_directory):
         os.makedirs(execution_directory)
